2a812c47b1d27153f10889f15cdfdaebafcbce9b
zORg <zorg-alex@ya.ru>
2022-12-03 15:43:59 +0200

    PlayerInput and Movement

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+	public Controls Controls;
+	public static InputController Instance;
+	public PlayerMovement playerMovement;
+	private IEnumerator _playerIinputCR;
+
+	private void OnEnable()
+	{
+		Controls = new Controls();
+		Instance = this;
+		if (playerMovement != null)
+			StartPlayerInputCR();
+	}
+
+	private void StartPlayerInputCR()
+	{
+		Controls.Player.Enable();
+		if (_playerIinputCR != null) StopCoroutine(_playerIinputCR);
+		_playerIinputCR = PlayerInputCR();
+		StartCoroutine(_playerIinputCR);
+	}
+	private void StopPlayerInput()
+	{
+		Controls.Player.Disable();
+		if (_playerIinputCR != null)
+			StopCoroutine(_playerIinputCR);
+		_playerIinputCR = null;
+	}
+
+	IEnumerator PlayerInputCR()
+	{
+		while (true)
+		{
+			yield return null;
+			playerMovement.Move(Controls.Player.Move.ReadValue<Vector2>());
+		}
+	}
+
+	public void EnablePlayerInput() => Controls.Player.Enable();
+	public void DisablePlayerInout() => Controls.Player.Disable();
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour, IMove, ISphereCoordinatesUser {
+	public float movementMultiplier;
+	new Rigidbody rigidbody;
+
+	public SphereCoordinates SphereCoordinates { get; set; }
+
+	private void OnEnable()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+	public void Move(Vector2 input)
+	{
+		Vector3 newPos = transform.localPosition + new Vector3(input.x * movementMultiplier, 0, input.y * movementMultiplier);
+		rigidbody.Move(newPos, SphereCoordinates.GetForwardHorizontalRotation(newPos, transform.forward));
+	}
+}
