df806b7a1834f67c05d94521c4c52d49c0d0ef6c
zORg <zorg-alex@ya.ru>
2022-12-06 14:01:16 +0200

    Added YBot and basic locomotion animations + mixamo import utility

---
+++ Assets/Scripts/Utility/MixamoImportTools.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MixamoImportTools : EditorWindow {
+	[MenuItem("Window/Mixamo Import Tools")]
+	private static void OpenEditorWindow() {
+		GetWindow<MixamoImportTools>().Show();
+	}
+	void FixNamingInAnimations() {
+		foreach (GameObject o in Selection.objects) {
+			ModelImporter modelImporter = (ModelImporter)AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(o));
+			var clips = modelImporter.defaultClipAnimations;
+			foreach (var c in clips) {
+				if (c.name == "mixamo.com") c.name = o.name;
+			}
+			modelImporter.clipAnimations = clips;
+		}
+	}
+	private void OnGUI()
+	{
+		if (GUILayout.Button("Fix naming in Animations"))
+		{
+			FixNamingInAnimations();
+		}
+	}
+}
